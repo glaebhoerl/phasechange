75e7295b695a1cf96a81751f456810db7402b326
Gábor Lehel <illissius@gmail.com>
2012-06-29 21:58:49 +0200

    bunch of comments, RULES, stuff

--- Data/PhaseChange.hs
+++ Data/PhaseChange.hs
@@ -3,11 +3,11 @@
 module Data.PhaseChange
     (
 -- * Kind @*@
-    PhaseChange (type Thawed, type Frozen), Mutable, Immutable, copy, thaw, freeze, updateWith,
+    PhaseChange (type Thawed, type Frozen), Mutable, Immutable, copy, thaw, freeze, frozen, updateWith,
 -- * Kind @* -> *@
-    M1(..), copy1, thaw1, freeze1, updateWith1,
+    M1(..), copy1, thaw1, freeze1, frozen1, updateWith1,
 -- * Kind @* -> * -> *@
-    M2(..), copy2, thaw2, freeze2, updateWith2
+    M2(..), copy2, thaw2, freeze2, frozen2, updateWith2
     )
     where
 
--- Data/PhaseChange/Internal.hs
+++ Data/PhaseChange/Internal.hs
@@ -16,6 +16,14 @@ import Control.Monad.ST.Class
 import GHC.Exts                (RealWorld)
 --import Control.Newtype
 
+-- you might think that unsafeThaw and unsafeFreeze (and for that matter thaw) don't need to
+-- be monadic, because after all they're just conceptually wrapping/unwrapping newtypes, and
+-- the argument of thaw is immutable so evaluating lazily should be fine.
+-- two things get in the way:
+--  - GHC's unsafeFreeze/unsafeThaw aren't actually pure, they mutate some bits to let the
+--    garbage collector know what's mutable and what's not; and
+--  - while it wouldn't break referential transparency directly, it would if you were to use
+--    the result of thaw/unsafeThaw in two different calls to runST. so we can't allow that.
 class (Thawed imm ~ mut, Frozen mut ~ imm) => PhaseChange (imm :: *) (mut :: * -> *) where
     type Thawed imm  :: * -> *
     type Frozen mut  :: *
@@ -38,26 +46,40 @@ instance PhaseChange imm (Thawed imm) => Immutable imm
 unsafeThaw :: (Immutable imm, MonadST mST, s ~ World mST) => imm -> mST (Thawed imm s)
 unsafeThaw = liftST . unsafeThawImpl
 {-# INLINABLE unsafeThaw #-}
+{-# SPECIALIZE unsafeThaw :: (Immutable imm, s ~ World (ST s)) => imm -> ST s (Thawed imm s) #-}
+{-# SPECIALIZE unsafeThaw :: Immutable imm => imm -> IO (Thawed imm RealWorld) #-}
+-- NOTE this is extremely delicate!
+-- With IO it only works if I pre-expand the World type family, with ST it
+-- only works if I don't. And if I use World directly in the original signature
+-- instead of naming it 's', everything changes again.
+-- In general the only way to figure it out seems to be trial and error.
+-- Otherwise GHC says things like: "RULE left-hand side too complicated to desugar",
+-- or sometimes "match_co baling out".
 
 unsafeFreeze :: (Mutable mut, MonadST mST, s ~ World mST) => mut s -> mST (Frozen mut)
 unsafeFreeze = liftST . unsafeFreezeImpl
 {-# INLINABLE unsafeFreeze #-}
+{-# SPECIALIZE unsafeFreeze :: (Mutable mut, s ~ World (ST s)) => mut s -> ST s (Frozen mut) #-}
+{-# SPECIALIZE unsafeFreeze :: (Mutable mut, s ~ RealWorld) => mut s -> IO (Frozen mut) #-}
 
 copy :: (Mutable mut, MonadST mST, s ~ World mST) => mut s -> mST (mut s)
 copy = liftST . copyImpl
 {-# INLINABLE copy #-}
 {-# SPECIALIZE copy :: (Mutable mut, s ~ World (ST s)) => mut s -> ST s (mut s) #-}
 {-# SPECIALIZE copy :: (Mutable mut, s ~ RealWorld) => mut s -> IO (mut s) #-}
--- NOTE this is extremely delicate!
--- With IO it only works if I pre-expand the World type family, with ST it
--- only works if I don't. And if I use World directly in the original signature
--- instead of naming it 's', everything changes again.
--- GHC says things like: "RULE left-hand side too complicated to desugar",
--- or sometimes "match_co baling out"
 
 thaw :: (Immutable imm, MonadST mST, s ~ World mST) => imm -> mST (Thawed imm s)
-thaw = copy <=< unsafeThaw
-{-# INLINABLE thaw #-}
+thaw = thawImpl
+{-# INLINE thaw #-}
+
+thawImpl :: (PhaseChange imm mut, MonadST mST) => imm -> mST (mut (World mST))
+thawImpl = copy <=< unsafeThaw
+{-# INLINABLE thawImpl #-}
+{-# SPECIALIZE thawImpl :: PhaseChange imm mut => imm -> ST s (mut (World (ST s))) #-}
+{-# SPECIALIZE thawImpl :: PhaseChange imm mut => imm -> IO (mut (World IO)) #-}
+-- need to do this ugly thaw/thawImpl thing because I couldn't find a way at all to get
+-- the SPECIALIZE to work otherwise
+-- (interestingly unsafeThaw has the same exact type signature and didn't run into problems...)
 
 freeze :: (Mutable mut, MonadST mST, s ~ World mST) => mut s -> mST (Frozen mut)
 freeze = unsafeFreeze <=< copy
@@ -67,26 +89,50 @@ freeze = unsafeFreeze <=< copy
 
 frozen :: Mutable mut => (forall s. ST s (mut s)) -> Frozen mut
 frozen m = runST $ unsafeFreeze =<< m
+{-# NOINLINE [1] frozen #-}
+-- {-# INLINABLE frozen #-}
+-- I don't see why these should conflict, but GHC says they do
 
-updateWith :: Immutable imm => (forall s. Thawed imm s -> ST s ()) -> imm -> imm
+-- really wanted to do this the other way around with Immutable and Thawed, but I found
+-- absolutely no way to get the RULES to work that way, not even the thaw/thawImpl trick
+updateWith :: Mutable mut => (forall s. mut s -> ST s ()) -> Frozen mut -> Frozen mut
 updateWith f a = runST $ do { m <- thaw a; f m; unsafeFreeze m }
-{-# INLINABLE updateWith #-}
+{-# NOINLINE [1] updateWith #-}
+-- {-# INLINABLE updateWith #-}
+
+type Maker   mut = forall s. ST s (mut s)
+type Updater mut = forall s. mut s -> ST s ()
+
+{-# RULES
+    "updateWith/frozen"
+        forall (m :: Maker mut) (f :: Updater mut).
+           updateWith f (frozen m) = frozen (m >>= \m' -> f m' >> return m');
+
+    "updateWith/updateWith"
+         forall (f :: Updater mut) (g :: Updater mut) i.
+              updateWith f (updateWith g i) = updateWith (\m -> f m >> g m) i
+  #-}
 
 updateWithResult :: Immutable imm => (forall s. Thawed imm s -> ST s a) -> imm -> (imm, a)
 updateWithResult f a = runST $ do { m <- thaw a; r <- f m; i <- unsafeFreeze m; return (i, r) }
 {-# INLINABLE updateWithResult #-}
 
+{- RULES
+    "updateWithResult/frozen"
+        forall (m :: Mutable mut => (forall s. ST s (mut s))) (f :: Immutable imm => (forall s. Thawed imm s -> ST s a)).
+           updateWithResult f (frozen m) = frozen (m >>= \m' -> f m' >> return m');
+  -}
+
 --updateManyWith :: (Immutable imm, Functor f) => (forall s. f (Thawed imm s) -> ST s ()) -> f imm -> f imm
 --updateManyWith f a = runST $ do { 
 
--- is this safe?
-readWith :: Mutable mut => (Frozen mut -> a) -> mut s -> ST s a
-readWith f a = do { i <- unsafeFreeze a; r <- return =<< (return $! (f $! i)); _ <- unsafeThaw i; return r }
-{-# INLINABLE readWith #-}
-
 --withPlus :: Immutable a => a -> (forall s. Thawed a s -> ST s (Thawed a s, b)) -> (a, b)
 
-
+{-
+let (foo, vec')   = updateWithResult asdf vec
+    (bar, vec'')  = updateWithResult bsdf vec'
+    (baz, vec''') = updateWithResult csdf vec''
+-}
 
 newtype M1 mut a s = M1 { unM1 :: mut s a }
 
@@ -113,6 +159,10 @@ thaw1 = liftM unM1 . thaw
 freeze1 :: (PhaseChange (imm a) (M1 mut a), MonadST mST, s ~ World mST) => mut s a -> mST (imm a)
 freeze1 = freeze . M1
 {-# INLINE freeze1 #-}
+
+frozen1 :: PhaseChange (imm a) (M1 mut a) => (forall s. ST s (mut s a)) -> imm a
+frozen1 m = frozen (liftM M1 m)
+{-# INLINE frozen1 #-}
 
 updateWith1 :: PhaseChange (imm a) (M1 mut a) => (forall s. mut s a -> ST s ()) -> imm a -> imm a
 updateWith1 f = updateWith (f . unM1)
@@ -144,6 +194,10 @@ thaw2 = liftM unM2 . thaw
 freeze2 :: (PhaseChange (imm a b) (M2 mut a b), MonadST mST, s ~ World mST) => mut s a b -> mST (imm a b)
 freeze2 = freeze . M2
 {-# INLINE freeze2 #-}
+
+frozen2 :: PhaseChange (imm a b) (M2 mut a b) => (forall s. ST s (mut s a b)) -> imm a b
+frozen2 m = frozen (liftM M2 m)
+{-# INLINE frozen2 #-}
 
 updateWith2 :: PhaseChange (imm a b) (M2 mut a b) => (forall s. mut s a b -> ST s ()) -> imm a b -> imm a b
 updateWith2 f = updateWith (f . unM2)
